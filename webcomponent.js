217be86e5bef876aace85f0784f6a7cf2e748711
SAPTechEd2019ain369 <52492960+SAPTechEd2019ain369@users.noreply.github.com>
2019-09-24 18:16:54 -0700

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -10,7 +10,7 @@
       </style>
     `;
 
-    d3Script.onload = () => 
+    d3Script.onload = () =>
 
     customElements.define('com-sap-teched-gauge-12', class Gauge extends HTMLElement {
 
@@ -22,42 +22,58 @@
             }
             catch{}
         }
-    
+
+        connectedCallback () {
+            const bcRect = this.getBoundingClientRect();
+            this._widgetHeight = bcRect.height;
+            this._widgetWidth = bcRect.width;
+            this.redraw();
+        }
+
         constructor() {
             super();
             //Constants
             if (!window._d3){
                 window._d3 = d3;
             }
-            
+
             this._shadowRoot = this.attachShadow({mode: 'open'});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.style.height = "100%";  //Beta Workaround
             this._svgContainer;
-            this.style.height = "100%";
 
             this._outerRad = 0.0;
             this._endAngleDeg = 0.0;
             this._endAngleDegMax = 145.0;
             this._startAngleDeg = -145.0;
+
+            //Guide Lines
+            this._ringColorCode = 'black';
+            this._guideOpacity = 0.75;
+            this._ringThickness = 5;
+            this._bracketThickness = 5;
+        };
+
+        onCustomWidgetResize(width, height) {
             const bcRect = this.getBoundingClientRect();
             this._widgetHeight = bcRect.height;
             this._widgetWidth = bcRect.width;
-            
-            //Guide Lines
-/*             this._ringColorCode = 'black';
-            this._guideOpacity = 0.75;
-            this._ringThickness = 5;
-            this._bracketThickness = 5; */
+            this.redraw();
+        }
 
+        redraw() {
             if (this._widgetHeight < this._widgetWidth){
                 this._widgetWidth = this._widgetHeight;
             }
 
-            this.redraw();
-        };
-
-        redraw() {
             if (!this._svgContainer){
+                this._svgContainer = window._d3.select(this._shadowRoot)
+                .append("svg:svg")
+                .attr("id", "gauge")
+                .attr("width", this._widgetWidth)
+                .attr("height", this._widgetWidth);
+            } else{
+                window._d3.select(this._shadowRoot).selectAll("*").remove();
                 this._svgContainer = window._d3.select(this._shadowRoot)
                 .append("svg:svg")
                 .attr("id", "gauge")
@@ -65,13 +81,13 @@
                 .attr("height", this._widgetWidth);
             }
 
-            var pi = Math.PI;  
+            var pi = Math.PI;
             this._outerRad = (this._widgetWidth)/2;
 
             var arcDef = window._d3.arc()
                 .innerRadius(0)
                 .outerRadius(this._outerRad);
-        
+
             var guageArc = this._svgContainer.append("path")
                 .datum({endAngle: this._endAngleDeg * (pi/180), startAngle: this._startAngleDeg * (pi/180)})
                 .style("fill", this._displayedColor)
@@ -80,11 +96,12 @@
                 .attr("d", arcDef)
                 .attr( "fill-opacity", this._gaugeOpacity );
 
-            /////////////////////////////////////////// 
+
+            ///////////////////////////////////////////
             //Lets build a border ring around the gauge
             ///////////////////////////////////////////
             var visRing = window._d3.select(this._shadowRoot).append("svg:svg").attr("width", "100%").attr("height", "100%");
-            
+
             var ringOuterRad = this._outerRad + ( -1 * this._ringThickness);  //Outer ring starts at the outer radius of the inner arc
 
             var ringArcDefinition = window._d3.arc()
@@ -97,7 +114,8 @@
                 .append("path")
                 .attr("d", ringArcDefinition)
                 .attr("fill", this._ringColorCode)
-                .attr("transform", "translate(" + this._outerRad + "," + this._outerRad + ")");                
+                .attr("transform", "translate(" + this._outerRad + "," + this._outerRad + ")");
+
 
             ///////////////////////////////////////////
             //Lets build a the start and end lines
@@ -107,25 +125,27 @@
             var lineFunction = window._d3.line()
                 .x(function(d) { return d.x; })
                 .y(function(d) { return d.y; });
-                
+
             var borderLines = this._svgContainer
                 .attr("width", this._widgetWidth).attr("height", this._widgetWidth) // Added height and width so line is visible
                 .append("path")
                 .attr("d", lineFunction(lineData))
                 .attr("stroke", this._ringColorCode)
                 .attr("stroke-width", this._bracketThickness)
-                .attr("fill", "none");            
+                .attr("fill", "none");
+
         };
 
-        //Helper function 
+
+        //Helper function
         endPoints (lineLength, lineAngle){
             var pi = Math.PI;
             var endX = this._outerRad + (lineLength * Math.sin(lineAngle * (pi/180)));
             var endY = this._outerRad - (lineLength * Math.cos(lineAngle * (pi/180)));
             return {x:endX, y:endY}
-        };        
-    
-    
+        };
+
+
     });
-        
+
 })();
